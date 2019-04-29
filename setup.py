29e9e395c40a18cac8e364cc5995e180bf632d06
HabebusRizmius <49805104+HabebusRizmius@users.noreply.github.com>
2019-04-29 13:26:21 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+    name='pylarms',
+    packages=['pylarms'],
+    version='0.0.1',
+    license='MIT',
+    description='Includes an alarm and timer.',
+    author='Habeb Rizmi',
+    author_email='habeb.rizmi@hotmail.com',
+    url='https://github.com/HabebusRizmius/pylarms',
+    download_url='https://github.com/HabebusRizmius/pylarms/archive/v_01.tar.gz',
+    keywords=['Timer', 'Alarm', 'Time'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        ],
+    )
